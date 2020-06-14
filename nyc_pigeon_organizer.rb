1c8889381121db4b1925ed6c1e463f294953b37a
Derek Turner <turner.derek.b@gmail.com>
2020-06-14 22:09:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,10 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.each_with_object({}) do |(key, value), final_array|
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !final_array[name]
+          final_array[name] = {}
+      end
+    end
+  end
 end
